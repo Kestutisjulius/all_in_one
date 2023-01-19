45c9a0bdaae915f749abeeb91db9d0431c3a8a42
Kęstutis Skrebė <kestutis.julius@gmail.com>
2023-01-19 19:04:25 +0200

    three box update

--- r1-app/src/App.jsx
+++ r1-app/src/App.jsx
@@ -1,14 +1,26 @@
 
 import './App.css';
 
+const bOStyle = {
+  color: 'yellow',
+  fontSize: '60px',
+  fontWeight: 'bold'
+};
+
+const BoxOne = ()=>{
+
+  return (<div className = "box" style = {bOStyle}>1</div>);
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
 
-        <p>
-          Edit Afrika
-        </p>
+      <BoxOne/>
+      <div className = "box">2</div>
+      <div className = "box">3</div>
+      
 
       </header>
     </div>
